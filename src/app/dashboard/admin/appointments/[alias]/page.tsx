afbeba5cd5bc37e0810fdee2ff263d1625c5b544
mahbub045 <mahbubrmn45@gmail.com>
2026-04-07 17:49:13 +0600

    feat: add Appointment and Patient details pages with corresponding containers and update sidebar navigation

---
+++ src/app/dashboard/admin/appointments/[alias]/page.tsx
@@ -0,0 +1,5 @@
+import AppointmentDetailsContainer from "@/components/Dashboard/Admin/Appointments/[Alias]";
+
+export default async function AppointmentDetailsPage() {
+  return <AppointmentDetailsContainer />;
+}
--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -39,6 +39,16 @@ const navItemsByRole: Record<
       label: "Doctors",
       icon: StethoscopeIcon,
     },
+    {
+      href: "/dashboard/admin/patients",
+      label: "Patients",
+      icon: Users,
+    },
+    {
+      href: "/dashboard/admin/appointments",
+      label: "Appointments",
+      icon: BriefcaseMedical,
+    },
   ],
   RECEPTIONIST: [
     { href: "/dashboard/receptionist", label: "Home", icon: Home },
